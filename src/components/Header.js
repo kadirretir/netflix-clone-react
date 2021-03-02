05cf2c80914d0a1f9442be7009047f04fd38853d
Kadir <kadirramazan344@gmail.com>
2021-03-02 07:37:39 +0300

    layout-structure

---
+++ src/components/Header.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import '../App.css';
+/* import '.../node_modules/bootstrap/dist/css/bootstrap.min.css'; */
+
+/* <div className="col ">
+<span className="logo">NETFLIX</span>
+</div>
+
+<div className="col">
+
+</div> */
+
+function Header() {
+
+    return (
+        <>
+        <header id="header">
+              <div className="container">
+                  <div className="logo">
+                   
+                  </div>
+
+                  <div className="introduction">
+                  <b>SINIRSIZ DİZİ VE FİLM</b>
+                  <button type="button">ŞİMDİ KATIL</button>
+                  <button type="button">OTURUM AÇ</button>
+                  </div>
+
+              </div>
+          </header>
+
+            <section id="movies-header">
+
+            <div className="container">
+
+                  <div className="row">
+                  <h1>Filmler</h1>
+                <p>
+                            Korkutucu, komik, dramatik, romantik ne türde olursa olsun etkileyici bir film kadar 
+                            duygularımızı harekete geçiren başka bir şey yok.
+                            Herkese hitap eden geniş bir film arşivi sizi bekliyor. 
+                    <a href="/">Şimdi ücretsiz izleyin</a>
+                </p>
+                  </div>
+
+            </div>
+
+            </section>
+            </>
+    );
+}
+
+export default Header;
---
+++ src/components/Movies.js
@@ -0,0 +1,29 @@
+import React from 'react';
+/* import '../App.css'; */
+/* import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; */
+
+
+function Movies () {
+
+    return (
+        <div className="container">
+
+            <div className="row">
+
+                <div className="col">
+
+                 <div className="movie-cap">
+                 <img src="https://occ-0-2706-2705.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABVNvJ1GxsaFBF66ys_cYvPBpAkmcIadlHjfGGyXVXNm8719Q0CIpwdndlAr5gQ3uEEBw7CGrY3rq3G7VBbGQcnpIvJM.jpg?r=802" alt="movies">
+                </img>
+                <h5>Kral Şakir Korsanlarlar Diyarı</h5>
+                 </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default Movies;
